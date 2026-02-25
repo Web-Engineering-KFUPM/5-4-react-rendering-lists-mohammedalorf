a38bf9cb5e2ec955c0e09b7fd41eb19337ce5880
Mohammed Alorf <mohammed1alorf@gmail.com>
2026-02-25 08:32:17 +0300

    TASK 4: implement toggle and delete interactions

--- 5-4-react-rendering-lists/src/components/TaskItem.jsx
+++ 5-4-react-rendering-lists/src/components/TaskItem.jsx
@@ -5,17 +5,22 @@ export default function TaskItem({ task, onToggle, onDelete }) {
   return (
     <li className="task">
       <label className="taskMain">
-        {/* TODO (TASK 4): Checkbox calls onToggle(task.id) on change
-            Also make it reflect task.isDone using checked={task.isDone} */}
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={task.isDone}
+          onChange={() => onToggle(task.id)}
+        />
 
         {!task.isDone && <DueBadge dueDate={task.dueDate} />}
 
         <span className="title">{task.title}</span>
       </label>
 
-      {/* TODO (TASK 4): Delete button calls onDelete(task.id) on click */}
-      <button className="ghost" aria-label="Delete task">
+      <button
+        className="ghost"
+        aria-label="Delete task"
+        onClick={() => onDelete(task.id)}
+      >
         ✕
       </button>
     </li>
